47d7a345d0ef76c63b23a6fbe3077bfac31915b0
ashitoshshinde2673 <47410988+ashitoshshinde2673@users.noreply.github.com>
2019-04-30 16:34:47 +0530

    Update firebase.js
    Removing the credentials of firebase from the code as it is public.

--- mineapp/mine2/src/config/firebase.js
+++ mineapp/mine2/src/config/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDam9r8j4-FzGI4O-JQ0I-fSn_NluBU1VI",
-  authDomain: "my-first-react-app-c0069.firebaseapp.com",
-  databaseURL: "https://my-first-react-app-c0069.firebaseio.com",
-  projectId: "my-first-react-app-c0069",
-  storageBucket: "my-first-react-app-c0069.appspot.com",
-  messagingSenderId: "21769890659"
+  apiKey: "***************************************",
+  authDomain: "****************************************",
+  databaseURL: "***************************************",
+  projectId: "*****************************************",
+  storageBucket: "*************************************",
+  messagingSenderId: "*********************************"
 };
 
 const fire = firebase.initializeApp(config);
